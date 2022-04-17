f43dfbb84fc194a5dc9a8d1dc7ca4178ce81a949
amirulpants <78654637+amiruldevkun@users.noreply.github.com>
2022-04-18 00:45:02 +0800

    wrong commit

---
+++ src/Mainc.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace home.linuxenv.EATM.src
+{
+    public class Mainc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Amirul's Bank and Finance!");
+            
+        }
+    }
+}
