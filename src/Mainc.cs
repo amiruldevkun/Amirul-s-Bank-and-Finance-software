d8121c18b5a90b0766b150dc2ad385f780699745
amiruldevkun <amirulpanda123456@gmail.com>
2022-04-22 00:01:07 +0800

    made some progress with the eatm stuff

--- src/Mainc.cs
+++ src/Mainc.cs
@@ -2,16 +2,80 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.IO;
 
 namespace EATM
 {
     public class Mainc
     {
+        private static void ScriptAnim(string ongoing, string finished) {
+            Console.WriteLine(ongoing);
+            using (var progress = new ProgressBar()) {
+                for (int i = 0; i < 100; i++) {
+                    progress.Report((double)i / 100);
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(finished);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Amirul Finance Business. What could we do for you today?");
-            Console
-            
+            Console.WriteLine("Welcome to Amirul Finance Business.");
+            // Console.WriteLine("Please enter your PIN number: ");
+            // int PIN = Convert.ToInt32(Console.ReadLine());
+            ScriptAnim("DATABASE TEST", "PRINTING RESULTS");
+            if(File.Exists("EATM.db"))
+            {
+                Console.WriteLine("Database file found.");
+                Console.WriteLine(Path.GetFullPath("EATM.db"));
+            }
+            else
+            {
+                Console.WriteLine("Database file not found.");
+                Console.WriteLine("Creating database file...");
+                EATM.databaseAccess db = new EATM.databaseAccess();
+                db.CreateConnection();
+                Console.WriteLine("Database file created.");
+                Console.WriteLine(Path.GetFullPath("EATM.db"));
+            }
+        }
+        public class enquiries {
+            public void enquired() {
+                Console.WriteLine("1. Deposit");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Balance Inquiry");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Please enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("You have chosen to deposit.");
+                        Console.WriteLine("Please enter the amount you want to deposit: ");
+                        int deposit = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Your deposit is successful.");
+                        break;
+                    case 2:
+                        Console.WriteLine("You have chosen to withdraw.");
+                        Console.WriteLine("Please enter the amount you want to withdraw: ");
+                        int withdraw = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Your withdraw is successful.");
+                        break;
+                    case 3:
+                        Console.WriteLine("You have chosen to check your balance.");
+                        Console.WriteLine("Your balance is: ");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for using Amirul Finance Business.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+            }
         }
     }
 }
--- src/databaseAccess.cs
+++ src/databaseAccess.cs
@@ -2,10 +2,77 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace EATM
 {
     public class databaseAccess
     {
+        public SQLiteConnection CreateConnection() {
+           
+            SQLiteConnection conn = new SQLiteConnection("Data Source=EATM.db; Version=3; New=True; Compress=True;");
+
+            try {
+                
+                conn.Open();
+                
+            } catch (Exception e) {
+                
+                Console.WriteLine(e.Message);
+            
+            }
+            return conn;
+        }
+            static void MandatoryTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string CreateTable = "CREATE TABLE EATM(name VARCHAR(100), PIN INT, balance FLOAT)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = CreateTable;
+            sqlite_cmd.ExecuteNonQuery();
+            
+        }
+
+        static void UserBalanceHistoryTable(SQLiteConnection conn, string tableName) {
+            SQLiteCommand sqlite_cmd;
+            string CreateTable = "CREATE TABLE" + tableName + "(balancehistory FLOAT)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = CreateTable;
+            sqlite_cmd.ExecuteNonQuery();
+        }
+
+        static void InsertData(SQLiteConnection conn)
+        {
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test Text ', 1);";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test1 Text1 ', 2);";
+            sqlite_cmd.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable(Col1, Col2) VALUES ('Test2 Text2 ', 3);";
+            sqlite_cmd.ExecuteNonQuery();
+
+
+            sqlite_cmd.CommandText = "INSERT INTO SampleTable1(Col1, Col2) VALUES ('Test3 Text3 ', 3);";
+            sqlite_cmd.ExecuteNonQuery();
+
+        }
+
+        static void ReadData(SQLiteConnection conn)
+        {
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+            while (sqlite_datareader.Read())
+            {
+                string myreader = sqlite_datareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            conn.Close();
+        }
     }
 }
